9e83db49899a1f18b0953ba4d7071a13e185add4
Your Name <you@example.com>
2026-07-30 00:36:58 +0600

    a

---
+++ main/templatetags/math_tags.py
@@ -0,0 +1,16 @@
+"""
+Simple math template tags for Django templates.
+"""
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def mul(value, arg):
+    """Multiply value by arg."""
+    try:
+        return int(value) * int(arg)
+    except (ValueError, TypeError):
+        return 0
